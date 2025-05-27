44d4fca1eef82a20fc39a22cb37bd0ef1b252d15
jpsa20 <A01737223@tec.mx>
2025-05-27 10:17:44 -0600

    Update line_follow.launch.py

--- puzzlebot_line_follower/launch/line_follow.launch.py
+++ puzzlebot_line_follower/launch/line_follow.launch.py
@@ -1,29 +1,32 @@
-#!/usr/bin/env python3
-import os
-
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description():
-    pkg_share = get_package_share_directory('puzzlebot_line_follower')
-    params_file = os.path.join(pkg_share, 'params', 'line_follower_params.yaml')
+    pkg = get_package_share_directory('puzzlebot_line_follower')
+    params = os.path.join(pkg, 'params', 'line_follower_params.yaml')
 
     return LaunchDescription([
-        # Nodo de visión: solo detección de línea
         Node(
             package='puzzlebot_line_follower',
             executable='line_light_detector',
             name='line_light_detector',
-            output='screen',
-            parameters=[params_file]
+            parameters=[params],
+            output='screen'
         ),
-        # Nodo de control: PID básico sin semáforo
+        Node(
+            package='puzzlebot_line_follower',
+            executable='yolov8_detector',
+            name='yolov8_detector',
+            parameters=[params],
+            output='screen'
+        ),
         Node(
             package='puzzlebot_line_follower',
             executable='line_follower_controller',
             name='line_follower_controller',
-            output='screen',
-            parameters=[params_file]
+            parameters=[params],
+            output='screen'
         ),
     ])
